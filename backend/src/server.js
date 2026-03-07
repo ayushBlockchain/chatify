1bb5333fd8ab3926d11dc6aa591a9db5c2e903a5
ayushBlockchain <ayushlashkan192@gmail.com>
2026-03-08 01:14:25 +0530

    fix absolute production path

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import path from "path"
-import { fileURLToPath } from "url"
 
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.route.js"
@@ -9,20 +8,19 @@ import messageRoutes from "./routes/message.route.js"
 dotenv.config()
 
 const app = express();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const PORT = process.env.PORT || 3000;
+
+// 🔥 THIS IS THE IMPORTANT LINE
+const rootDir = path.resolve();
 
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 
 if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../../frontend/dist")));
+    app.use(express.static(path.join(rootDir,"frontend","dist")));
 
-    app.get("*",(_,res)=>{
-        res.sendFile(path.join(__dirname,"../../frontend","dist","index.html"));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(rootDir,"frontend","dist","index.html"));
     });
 }
 
