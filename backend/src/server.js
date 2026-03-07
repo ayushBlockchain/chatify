eec899738990cebadd226faede2484019b54bac7
ayushBlockchain <ayushlashkan192@gmail.com>
2026-03-08 01:10:07 +0530

    fix correct production path

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import path from "path"
+import { fileURLToPath } from "url"
 
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.route.js"
@@ -8,22 +9,21 @@ import messageRoutes from "./routes/message.route.js"
 dotenv.config()
 
 const app = express();
-const __dirname = path.resolve();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 3000;
-
 
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 
-//make ready for deployment 
 if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+    app.use(express.static(path.join(__dirname,"../../frontend/dist")));
 
     app.get("*",(_,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+        res.sendFile(path.join(__dirname,"../../frontend","dist","index.html"));
     });
 }
-
 
 app.listen(PORT, ()=>console.log(`server is running on port ${PORT}`))
