eb8af941112c08ebcd6d65fe723c7d73c089448b
ayushBlockchain <ayushlashkan192@gmail.com>
2026-03-11 14:02:59 +0530

    fix production build

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -19,6 +19,7 @@ const userSchema = new mongoose.Schema(
     profilePic: {
       type: String,
       default: "",
+      allowNull:true,
     },
   },
   { timestamps: true } // createdAt & updatedAt
